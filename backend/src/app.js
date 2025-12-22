02afd1f36c971a237c3b6a9e622b2de3388adbfe
Sayantan <sayantanbharati611@gmail.com>
2025-12-22 19:27:10 +0530

    public insted ..public in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,6 +35,7 @@ app.use("/api/chat", chatRoutes);
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
 module.exports = app;
