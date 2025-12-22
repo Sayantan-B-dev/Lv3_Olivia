1ae1a3ca3a6b397e396b85977ffe1d06fedf4aa4
Sayantan <sayantanbharati611@gmail.com>
2025-12-22 18:45:38 +0530

    app2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const path=require("path")
+//const path=require("path")
 /* Routes */
 const authRoutes = require("./routes/auth.routes");
 const chatRoutes = require("./routes/chat.routes");
@@ -25,7 +25,7 @@ app.use(
 /* ========================================= */
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'../public')))
+//app.use(express.static(path.join(__dirname,'../public')))
 
 /* ========================================= */
 /* 🚦 API ROUTES                             */
@@ -34,7 +34,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 
 
-app.get("*name",(req,res)=>{
-  res.sendFile(path.join(__dirname,'../public/index.html'))
-})
+// app.get("*name",(req,res)=>{
+//   res.sendFile(path.join(__dirname,'../public/index.html'))
+// })
 module.exports = app;
