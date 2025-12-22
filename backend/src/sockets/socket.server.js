1b2cdd58ac8df02f6fa9a70bab702585a6653c8b
Sayantan <sayantanbharati611@gmail.com>
2025-12-22 20:03:49 +0530

    .

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -11,7 +11,11 @@ const { createMemory, queryMemory } = require("../services/vector.service");
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        process.env.FRONTEND_URL,
+        "http://localhost:5173",
+        "https://olivia-chatbot.onrender.com"
+      ],
       credentials: true
     }
   });
